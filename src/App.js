6976827a516ca1ea17f4d604966d7208f9916d09
Armance Mousset <mousset.armance@gmail.com>
2023-04-03 18:36:39 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./styles/global.scss";
 
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      hi{" "}
+      <iframe
+        width="100%"
+        height="300"
+        scrolling="no"
+        frameborder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/922799797&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+      ></iframe>
+    </>
+  );
 }
 
 export default App;
