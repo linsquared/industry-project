cee1e0ba08180f2aa1bd5e899d76f44e76e8c5a2
Lin Lin <lin2013lin@gmail.com>
2023-04-03 20:29:13 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/global.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./assets/components/Header/Header";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
